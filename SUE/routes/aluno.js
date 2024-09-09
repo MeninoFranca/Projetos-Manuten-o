a51fc68b05d69becab10c567817b1607f547f594
Aluno SENAI LAURO <aluno@SENAI-LAURO.LOCAL>
2024-09-09 20:44:11 -0300

    Conceito put em aluno

--- SUE/routes/aluno.js
+++ SUE/routes/aluno.js
@@ -1,42 +1,56 @@
 const express = require("express");
-const { Aluno } = require("../database/aluno"); 
+const { Aluno } = require("../database/aluno");
 const router = express.Router();
 
-router.get('/aluno', async (req, res) => {
-    try {
-        const alunos = await Aluno.findAll({
-            raw: true
-        });
-        res.send(alunos);
-    } catch (error) {
-        console.error("Erro ao buscar alunos:", error);
-        res.status(500).json({ error: "Erro ao buscar alunos." });
-    }
+router.get("/aluno", async (req, res) => {
+  try {
+    const alunos = await Aluno.findAll({
+      raw: true,
+    });
+    res.send(alunos);
+  } catch (error) {
+    console.error("Erro ao buscar alunos:", error);
+    res.status(500).json({ error: "Erro ao buscar alunos." });
+  }
 });
 
-router.post("/aluno/criar",async(req,res)=>{
-    try {
-        const{
-            nome_aluno,
-            Num_Matricula,
-            Estado_Matricula
-        } = req.body;
+router.post("/aluno/criar", async (req, res) => {
+  try {
+    const { nome_aluno, Num_Matricula, Estado_Matricula } = req.body;
 
-        await Aluno.create({
-            nome_aluno,
-            Num_Matricula,
-            Estado_Matricula
-        })
+    await Aluno.create({
+      nome_aluno,
+      Num_Matricula,
+      Estado_Matricula,
+    });
 
-        res.status(201)
-        console.log("usuário criado com sucesso")
-    } catch (error) {
-        res.status(501).json(error)
-        console.error(error)
+    res.status(201);
+    console.log("usuário criado com sucesso");
+  } catch (error) {
+    res.status(501).json(error);
+    console.error(error);
+  }
+});
+
+router.put("/aluno/editar/:id", async (req, res) => {
+  try {
+    const { nome_aluno, Num_Matricula, Estado_Matricula} = req.body;
+    const id = req.params.id;
+
+    const aluno = await Aluno.findByPk(id);
+
+    if (!aluno) {
+      res.status(404).json("tabela nao encontrada");
     }
-})
 
-router.post("")
+    aluno.nome_aluno = nome_aluno;
+    aluno.Num_Matricula = Num_Matricula;
+    aluno.Estado_Matricula = Estado_Matricula;
 
+    await aluno.save();
+  } catch (error) {
+      res.status(501).json(error)
+  }
+});
 
 module.exports = router;
